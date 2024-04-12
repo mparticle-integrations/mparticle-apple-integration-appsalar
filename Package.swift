a6aa9907c99d8dfd50dddbef1cbdc351be6b901a
Ben Baron <bbaron@mparticle.com>
2024-04-12 12:56:44 -0500

    create new Xcode project and fix tests

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "mParticle-Singular",
-    platforms: [ .iOS(.v9) ],
+    platforms: [ .iOS(.v12) ],
     products: [
         .library(
             name: "mParticle-Singular",
